069ea988e1c3be14c0bd3cba14ec2cf3afc06ffe
Hongfei Yang <hongfei.yang@meeco.me>
2020-08-12 12:30:45 +1000

    added file chunking, fixed a bug in encryption artefacts serialisation

--- lib/encryption/encryption_result.dart
+++ lib/encryption/encryption_result.dart
@@ -25,6 +25,11 @@ class EncryptionResult {
   /// Decode a serialized encrypted string into its components.
   EncryptionResult.fromSerialized(String serializedPayload) {
     final decomposedPayload = serializedPayload.split(".");
+    // Payload should contain the following parts:
+    // 0. strategy: a [EncryptionStrategy]
+    // 1. cipher text: a base64url encoded [Uint8List]
+    // 2. serialised encryption artefacts: a string (optional)
+    // 3. serialised derivation artefacts: a string (optional)
     if (!(decomposedPayload.length == 3 || decomposedPayload.length == 4)) {
       throw Exception(
           'Serialised encryption should contain 3 or 4 parts, depending on version');
@@ -32,6 +37,8 @@ class EncryptionResult {
     strategy = encryptionStrategyFromString(decomposedPayload[0]);
     final cipherText = decomposedPayload[1];
     this.cipherText = base64Url.decode(cipherText);
+    this.encryptionArtefacts =
+        EncryptionArtefacts.fromSerialized(decomposedPayload[2]);
   }
 
   /// Converts the encryption result into Cryppo's serialization format for transfer over the wire.
